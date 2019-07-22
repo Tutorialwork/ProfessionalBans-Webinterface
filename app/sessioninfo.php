752ba03448159d0583abade7bfe92f040670ff30
Tutorialwork <tutorialworktv@gmail.com>
2019-07-22 14:09:48 +0200

    removed installer and updated sessioninfo

--- app/sessioninfo.php
+++ app/sessioninfo.php
@@ -8,10 +8,16 @@ if(isset($request["token"])){
   $access = new TokenHandler($request["token"]);
   if($access->username != null){
       if(isset($request["usertoken"])){
-        $response["status"] = 1;
-        $response["msg"] = "OK";
-        $response["username"] = $access->username;
-        $response["uuid"] = $access->uuid;
+        $info = new TokenHandler($request["usertoken"]);
+        if($info->username != null){
+          $response["status"] = 1;
+          $response["msg"] = "OK";
+          $response["username"] = $info->username;
+          $response["uuid"] = $info->uuid;
+        } else {
+          $response["status"] = 0;
+          $response["msg"] = "Token not found";
+        }
       } else {
         $response["status"] = 0;
         $response["msg"] = "No token informations was requsted";
