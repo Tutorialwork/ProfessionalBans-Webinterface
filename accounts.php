840c97fc184046af3036a60f07623db456f54288
Tutorialwork <tutorialworktv@gmail.com>
2019-11-10 23:38:01 +0100

    Added function to delete and create webaccounts

--- accounts.php
+++ accounts.php
@@ -4,6 +4,75 @@
           showModalRedirect("ERROR", "Fehler", "Der Zugriff auf diese Seite wurde verweigert.", "index.php");
           exit;
         }
+        if(isset($_GET["delete"]) && isset($_GET["name"])){
+          if(!isset($_GET["confirmed"])){
+            if(!empty($_GET["name"])){
+              ?>
+              <script>
+                $.sweetModal.defaultSettings.confirm.yes.label = "Löschen";
+                $.sweetModal.defaultSettings.confirm.cancel.label = "Abbrechen";
+                $.sweetModal.confirm('Möchtest du wirklich <strong><?php echo $_GET["name"]; ?></strong> löschen?', function() {
+                  var xhttp = new XMLHttpRequest();
+                  xhttp.open("GET", "accounts.php?delete&name=<?php echo $_GET["name"]; ?>&confirmed");
+                  xhttp.send();
+                  $.sweetModal({
+                    content: '<strong><?php echo $_GET["name"]; ?></strong> wurde erfolgreich gelöscht.',
+                    icon: $.sweetModal.ICON_SUCCESS,
+                    onClose: function(){
+                      window.location = "accounts.php";
+                    }
+                  });
+                });
+              </script>
+              <?php
+            } else {
+              header("Location: accounts.php");
+            }
+          } else {
+            require("./mysql.php");
+            $stmt = $mysql->prepare("DELETE FROM accounts WHERE USERNAME = :user");
+            $stmt->execute(array(":user" => $_GET["name"]));
+          }
+        }
+        if(isset($_POST["submit"]) && isset($_SESSION["CSRF"])){
+          if(!empty($_POST["username"]) && !empty($_POST["mcusername"])){
+            if($_POST["CSRFToken"] != $_SESSION["CSRF"]){
+              showModal("ERROR", "CSRF Fehler", "Deine Sitzung ist abgelaufen. Versuche die Seite erneut zu öffnen.");
+            } else {
+              require("./mysql.php");
+              $stmt = $mysql->prepare("SELECT * FROM bans WHERE NAME = :request");
+              $stmt->execute(array(":request" => $_POST["mcusername"]));
+              $row = $stmt->fetch();
+              if(isPlayerExists($row["UUID"])){
+                $stmt = $mysql->prepare("SELECT * FROM accounts WHERE UUID = :request");
+                $stmt->execute(array(":request" => $row["UUID"]));
+                if($stmt->rowCount() == 0){
+                  $stmt = $mysql->prepare("SELECT * FROM accounts WHERE USERNAME = :request");
+                  $stmt->execute(array(":request" => $_POST["username"]));
+                  if($stmt->rowCount() == 0){
+                    $stmt = $mysql->prepare("INSERT INTO accounts(UUID, USERNAME, PASSWORD, RANK, GOOGLE_AUTH, AUTHCODE) 
+                    VALUES (:uuid, :name, :hash, :rank, 'null', 'initialpassword')");
+                    $klartext = generateRandomString();
+                    $hash = password_hash($klartext, PASSWORD_BCRYPT);
+                    $stmt->execute(array(":uuid" => $row["UUID"], ":name" => $_POST["username"], ":hash" => $hash, ":rank" => $_POST["rang"]));
+                    showModalRedirect("SUCCESS", "Erfolgreich", "Es wurde erfolgreich für <strong>".htmlspecialchars($_POST["mcusername"])."</strong> ein Webaccount angelegt mit dem temporären Passwort: <strong>".$klartext."</strong>", "accounts.php");
+                  } else {
+                    showModal("ERROR", "Fehler", "Dieser Username ist bereits vergeben");
+                  }
+                } else {
+                  showModal("ERROR", "Fehler", "Dieser Spieler hat bereits einen Webaccount");
+                }
+              } else {
+                showModal("ERROR", "Fehler", "Dieser Spieler hat das Netzwerk noch nie betreten");
+              }
+            }
+          } else {
+            showModal("ERROR", "Fehler", "Damit ein neuer Account erstellt werden kann werden alle Felder benötigt");
+          }
+        } else {
+          //Erstelle Token wenn Formular nicht abgesendet wurde
+          $_SESSION["CSRF"] = generateRandomString(25);
+        }
         ?>
         <div class="flex-container animated fadeIn">
           <div class="flex item-1">
@@ -43,10 +112,9 @@
                ?>
             </table>
           </div>
-          <!--
           <div class="flex item-2 sidebox">
             <h1>Account erstellen</h1>
-            <form action="bans.php" method="post">
+            <form action="accounts.php" method="post">
               <input type="hidden" name="CSRFToken" value="<?php echo $_SESSION["CSRF"]; ?>">
               <input type="text" name="username" placeholder="Username" required><br>
               <input type="text" name="mcusername" placeholder="Minecraft Username" required><br>
@@ -58,13 +126,7 @@
               <button type="submit" name="submit">Account erstellen</button>
             </form>
           </div>
-        -->
         </div>
-        <script type="text/javascript">
-        function rankChangeModal(name){
-
-        }
-        </script>
       </div>
     </div>
   </body>
