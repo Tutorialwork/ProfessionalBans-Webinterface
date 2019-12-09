f3b823e46727c1e14d7c1b101d52a39cc74f8f62
Justin Klüver <justin.k@justplayer.de>
2019-12-09 11:30:10 +0100

    Optimised code

--- recovery.php
+++ recovery.php
@@ -1,94 +1,101 @@
+<?php
+require("./datamanager.php");
+$error = "";
+$success = "";
+if (isset($_POST["submit"])) {
+  $stmt = MySQLWrapper()->prepare("SELECT * FROM accounts WHERE USERNAME = :user");
+  $stmt->bindParam(":user", $_POST["mcusername"], PDO::PARAM_STR);
+  $stmt->execute();
+  $count = $stmt->rowCount();
+  if ($count == 1) {
+    setAuthCodeByMCName($_POST["mcusername"], generateRandomString(25));
+    header("Location: recovery.php?verify=" . htmlspecialchars($_POST["mcusername"]));
+  } else {
+    $error = "Es gibt kein Account mit diesem Minecraft Username.";
+  }
+}
+?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Passwort vergessen</title>
-    <link rel="stylesheet" href="css/login.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" type="image/x-icon" href="css/favicon.ico">
-  </head>
-  <body>
 
-    <?php
-    require("./datamanager.php");
-    require("./mysql.php");
-    if(isset($_POST["submit"])){
-      $stmt = $mysql->prepare("SELECT * FROM accounts WHERE USERNAME = :user");
-      $stmt->bindParam(":user", $_POST["mcusername"], PDO::PARAM_STR);
-      $stmt->execute();
-      $count = $stmt->rowCount();
-      if($count == 1){
-        setAuthCodeByMCName($_POST["mcusername"], generateRandomString(25));
-        header("Location: recovery.php?verify=".htmlspecialchars($_POST["mcusername"]));
-      } else {
-        ?>
-        <div class="error">
-          <h4>Es gibt kein Account mit diesem Minecraft Username.</h4>
-        </div>
-        <?php
-      }
-    }
-    if(isset($_GET["verify"])){
-      ?>
-      <form class="login" action="recovery.php?verify=<?php echo $_GET["verify"]; ?>&token=<?php echo getAuthCodeByMCName($_GET["verify"]); ?>" method="post">
-        <?php
-        if(isset($_POST["pwsubmit"])){
-          if(isAuth($_GET["verify"])){
-            if(getAuthCodeByMCName($_GET["verify"]) == $_GET["token"]){
-              if($_POST["pw"] == $_POST["pw2"]){
+<head>
+  <meta charset="utf-8">
+  <title>Passwort vergessen</title>
+  <link rel="stylesheet" href="css/login.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" type="image/x-icon" href="css/favicon.ico">
+</head>
+
+<body>
+
+  <?php
+  if (isset($_GET["verify"])) {
+    ?>
+    <form class="login" action="recovery.php?verify=<?php echo $_GET["verify"]; ?>&token=<?php echo getAuthCodeByMCName($_GET["verify"]); ?>" method="post">
+      <?php
+        if (isset($_POST["pwsubmit"])) {
+          if (isAuth($_GET["verify"])) {
+            if (getAuthCodeByMCName($_GET["verify"]) == $_GET["token"]) {
+              if ($_POST["pw"] == $_POST["pw2"]) {
                 resetAuthStatus($_GET["verify"]);
                 updatePasswordByMCName($_GET["verify"], $_POST["pw"]);
                 ?>
-                <div class="success">
-                  <h4>Das Passwort wurde erfolgreich geändert. Du kannst dich jetzt <a href="login.php">hier</a> einloggen.</h4>
-                </div>
-                <?php
+              <div class="success">
+                <h4>Das Passwort wurde erfolgreich geändert. Du kannst dich jetzt <a href="login.php">hier</a> einloggen.</h4>
+              </div>
+            <?php
+                    } else {
+                      ?>
+              <div class="error">
+                <h4>Die Passwörter stimmen nicht überein.</h4>
+              </div>
+            <?php
+                    }
+                  } else {
+                    ?>
+            <div class="error">
+              <h4>Ein Fehler ist aufgetreten.</h4>
+            </div>
+          <?php
+                }
               } else {
                 ?>
-                <div class="error">
-                  <h4>Die Passwörter stimmen nicht überein.</h4>
-                </div>
-                <?php
-              }
-            } else {
-              ?>
-              <div class="error">
-                <h4>Ein Fehler ist aufgetreten.</h4>
-              </div>
-              <?php
-            }
-          } else {
-            ?>
-            <div class="error">
-              <h4>Du musst zuerst deine Identität bestätigen.</h4>
-            </div>
-            <?php
+          <div class="error">
+            <h4>Du musst zuerst deine Identität bestätigen.</h4>
+          </div>
+      <?php
           }
         }
-         ?>
-        <h1><i class="fas fa-user-check"></i> Bestätigen</h1>
-        <p>Bitte bestätige jetzt deine Identität, indem du auf dem Minecraft Netzwerk folgenden Befehl eingibst. Anschließend kannst du unten ein neues Passwort festlegen.</p>
-        <input type="text" name="cmd" placeholder="Befehl" value="/webverify <?php echo getAuthCodeByMCName($_GET["verify"]); ?>"><br>
-        <p>Neues Passwort festlegen</p>
-        <input type="password" name="pw" placeholder="Neues Passwort" minlength="6" required>
-        <input type="password" name="pw2" placeholder="Neues Passwort bestätigen" minlength="6" required>
-        <button type="submit" name="pwsubmit">Passwort ändern</button>
-      </form>
-      <?php
-    } else {
-      //Warte auf Anfrage...
-      ?>
-      <form class="login" action="recovery.php" method="post">
-        <h1 class="small-heading"><i class="fas fa-key"></i> Passwort vergessen</h1>
-        <p>Gebe deinen Minecraft Username ein. Dieser kann sich möglicherweise von deinem Username im Webinterface unterscheiden.</p>
-        <input type="text" name="mcusername" placeholder="Minecraft Username" maxlength="16" minlength="3" required><br>
-        <button type="submit" name="submit">Bestätigen</button><br><br><br>
-        <a href="login.php"><i class="fas fa-arrow-left"></i> Zurück</a>
-      </form>
-      <?php
-    }
-     ?>
+        ?>
+      <h1><i class="fas fa-user-check"></i> Bestätigen</h1>
+      <p>Bitte bestätige jetzt deine Identität, indem du auf dem Minecraft Netzwerk folgenden Befehl eingibst. Anschließend kannst du unten ein neues Passwort festlegen.</p>
+      <input type="text" name="cmd" placeholder="Befehl" value="/webverify <?php echo getAuthCodeByMCName($_GET["verify"]); ?>"><br>
+      <p>Neues Passwort festlegen</p>
+      <input type="password" name="pw" placeholder="Neues Passwort" minlength="6" required>
+      <input type="password" name="pw2" placeholder="Neues Passwort bestätigen" minlength="6" required>
+      <button type="submit" name="pwsubmit">Passwort ändern</button>
+    </form>
+  <?php
+  } else {
+    //Warte auf Anfrage...
+    ?>
+    <form class="login" action="recovery.php" method="post">
+      <?php if (!empty($error)) { ?>
+        <div class="error">
+          <h4><?= $error; ?></h4>
+        </div>
+      <?php } ?>
+      <h1 class="small-heading"><i class="fas fa-key"></i> Passwort vergessen</h1>
+      <p>Gebe deinen Minecraft Username ein. Dieser kann sich möglicherweise von deinem Username im Webinterface unterscheiden.</p>
+      <input type="text" name="mcusername" placeholder="Minecraft Username" maxlength="16" minlength="3" required><br>
+      <button type="submit" name="submit">Bestätigen</button><br><br><br>
+      <a href="login.php"><i class="fas fa-arrow-left"></i> Zurück</a>
+    </form>
+  <?php
+  }
+  ?>
 
-  </body>
+</body>
+
 </html>
--- resetpassword.php
+++ resetpassword.php
@@ -1,93 +1,78 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Passwort setzen</title>
-    <link rel="stylesheet" href="css/login.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" type="image/x-icon" href="css/favicon.ico">
-  </head>
-  <body>
-    <form class="login" action="resetpassword.php?name=<?php echo $_GET["name"] ?>" method="post">
-      <?php
-      session_start();
-      if(!isset($_SESSION['username'])){
-        header("Location: login.php");
-        exit;
-      }
-      if(!isset($_GET["name"])){
-        ?>
-        <div class="error">
-          <h4>Es wurde keine Anfrage gestellt.</h4>
-        </div>
-        <?php
-        exit;
-      }
-      if(isset($_POST["submit"]) && isset($_POST["CSRFToken"])){
-        require("./mysql.php");
-        if($_POST["CSRFToken"] != $_SESSION["CSRF"]){
-          ?>
-          <div class="error">
-            <h4>Deine Sitzung ist abgelaufen. Versuche die Seite erneut zu öffnen.</h4>
-          </div>
-          <?php
-          exit;
-        }
-        if($_POST['pw1'] == $_POST['pw2']){
-          $stmt = $mysql->prepare("SELECT * FROM accounts WHERE USERNAME = :username");
+<?php
+session_start();
+require_once("datamanager.php");
+
+$error = "";
+if (!isset($_GET["name"])) {
+  ?>
+  <div class="error">
+    <h4>Es wurde keine Anfrage gestellt.</h4>
+  </div>
+<?php
+  exit;
+}
+if (isset($_POST["submit"]) && isset($_POST["CSRFToken"])) {
+  if ($_POST["CSRFToken"] != $_SESSION["CSRF"]) {
+    $error = "Deine Sitzung ist abgelaufen. Versuche die Seite erneut zu öffnen.";
+  } else
+    if ($_POST['pw1'] == $_POST['pw2']) {
+    $stmt = MySQLWrapper()->prepare("SELECT * FROM accounts WHERE USERNAME = :username");
+    $stmt->bindParam(":username", $_GET["name"], PDO::PARAM_STR);
+    $stmt->execute();
+
+    if ($row = $stmt->fetch()) {
+      $stmt = MySQLWrapper()->prepare("SELECT AUTHCODE FROM accounts WHERE USERNAME = :username");
+      $stmt->bindParam(":username", $_GET["name"], PDO::PARAM_STR);
+      $stmt->execute();
+      $data = 0;
+      while ($row = $stmt->fetch()) {
+        if ($row["AUTHCODE"] == "initialpassword") {
+          $PWHash = password_hash($_POST['pw1'], PASSWORD_BCRYPT);
+          $stmt = MySQLWrapper()->prepare("UPDATE accounts SET PASSWORD=:hash, AUTHCODE='null' WHERE USERNAME=:username");;
+          $stmt->bindParam(":hash", $PWHash, PDO::PARAM_STR);
           $stmt->bindParam(":username", $_GET["name"], PDO::PARAM_STR);
           $stmt->execute();
-          $data = 0;
-          while($row = $stmt->fetch()){
-            $data++;
-          }
-          if($data == 1){
-            $stmt = $mysql->prepare("SELECT AUTHCODE FROM accounts WHERE USERNAME = :username");
-            $stmt->bindParam(":username", $_GET["name"], PDO::PARAM_STR);
-            $stmt->execute();
-            $data = 0;
-            while($row = $stmt->fetch()){
-              if($row["AUTHCODE"] == "initialpassword"){
-                $PWHash = password_hash($_POST['pw1'], PASSWORD_BCRYPT);
-                $stmt = $mysql->prepare("UPDATE accounts SET PASSWORD=:hash, AUTHCODE='null' WHERE USERNAME=:username");;
-                $stmt->bindParam(":hash", $PWHash, PDO::PARAM_STR);
-                $stmt->bindParam(":username", $_GET["name"], PDO::PARAM_STR);
-                $stmt->execute();
-                header("Location: index.php");
-              } else {
-                ?>
-                <div class="error">
-                  <h4>Das Passwort für diesen Account kann nicht gesetzt werden.</h4>
-                </div>
-                <?php
-              }
-            }
-          } else {
-            ?>
-            <div class="error">
-              <h4>Dieser Account existiert nicht.</h4>
-            </div>
-            <?php
-          }
+          header("Location: index.php");
         } else {
-          ?>
-          <div class="error">
-            <h4>Die Passwörter stimmen nicht überein.</h4>
-          </div>
-          <?php
+          $error = "Das Passwort für diesen Account kann nicht gesetzt werden.";
         }
-      } else {
-        //Erstelle Token wenn Formular nicht abgesendet wurde
-        require("datamanager.php");
-        $_SESSION["CSRF"] = generateRandomString(25);
       }
-       ?>
-      <h1 id="pw"><i class="fas fa-key"></i> Passwort setzen</h1>
-      <input type="hidden" name="CSRFToken" value="<?php echo $_SESSION["CSRF"]; ?>">
-      <input type="password" name="pw1" placeholder="Passwort" minlength="6" autocomplete="new-password" required><br>
-      <input type="password" name="pw2" placeholder="Passwort bestätigen" minlength="6" autocomplete="new-password" required><br>
-      <button type="submit" name="submit">Passwort setzen</button><br>
-    </form>
-  </body>
+    } else {
+      $error = "Dieser Account existiert nicht.";
+    }
+  } else {
+    $error = "Die Passwörter stimmen nicht überein.";
+  }
+} else {
+  //Erstelle Token wenn Formular nicht abgesendet wurde
+  $_SESSION["CSRF"] = generateRandomString(25);
+}
+?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+  <meta charset="utf-8">
+  <title>Passwort setzen</title>
+  <link rel="stylesheet" href="css/login.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" type="image/x-icon" href="css/favicon.ico">
+</head>
+
+<body>
+  <form class="login" action="resetpassword.php?name=<?php echo $_GET["name"] ?>" method="post">
+    <?php if (!empty($error)) { ?>
+      <div class="error">
+        <h4><?= $error; ?></h4>
+      </div>
+    <?php } ?>
+    <h1 id="pw"><i class="fas fa-key"></i> Passwort setzen</h1>
+    <input type="hidden" name="CSRFToken" value="<?php echo $_SESSION["CSRF"]; ?>">
+    <input type="password" name="pw1" placeholder="Passwort" minlength="6" autocomplete="new-password" required><br>
+    <input type="password" name="pw2" placeholder="Passwort bestätigen" minlength="6" autocomplete="new-password" required><br>
+    <button type="submit" name="submit">Passwort setzen</button><br>
+  </form>
+</body>
+
 </html>
