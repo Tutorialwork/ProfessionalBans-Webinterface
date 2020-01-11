237fb348b3ea8e3441bb5b5db95f5dff2d35ee50
Manuel <tutorialworktv@gmail.com>
2020-01-11 16:13:05 +0100

    Added player online check

--- app/messages.php
+++ app/messages.php
@@ -1,14 +1,7 @@
 <?php
 require("tokenhandler.php");
 require("../mysql.php");
-function getNameByUUID($uuid){    
-    require("../mysql.php");
-    $stmt = $mysql->prepare("SELECT * FROM bans WHERE UUID = :uuid");
-    $stmt->bindParam(":uuid", $uuid, PDO::PARAM_STR);
-    $stmt->execute();
-    $row = $stmt->fetch();
-    return $row["NAME"];
-}
+require("../datamanager.php");
 function getOnlinePlayers(){
   require("../mysql.php");
   $stmt = $mysql->prepare("SELECT * FROM bans WHERE ONLINE_STATUS = 1");
@@ -34,24 +27,47 @@ $request = json_decode($JSONRequest, TRUE);
 if(isset($request["token"])){
   $access = new TokenHandler($request["token"]);
   if($access->username != null){
-    if(isset($request["send"]) && isset($request["to"]) && isset($request["onlinecheck"])){
-        $stmt = $mysql->prepare("INSERT INTO privatemessages (SENDER, RECEIVER, MESSAGE, STATUS, DATE) VALUES (:sender, :to, :msg, :status, :now)");
-        $stmt->bindParam(":sender", $access->uuid, PDO::PARAM_STR);
+    if(isset($request["send"]) && isset($request["to"])){
+        $uuid = NameResolve($request["to"]);
+        $onlinestmt = $mysql->prepare("SELECT * FROM apptokens WHERE UUID = :uuid");
+        $onlinestmt->execute(array(":uuid" => $uuid));
 
-        $stmt2 = $mysql->prepare("SELECT * FROM bans WHERE NAME = :user");
-        $stmt2->bindParam(":user", $request["to"], PDO::PARAM_STR);
-        $stmt2->execute();
-        $row2 = $stmt2->fetch();
-        
-        $stmt->bindParam(":to", $row2["UUID"], PDO::PARAM_STR);
-        $stmt->bindParam(":msg", $request["send"], PDO::PARAM_STR);
-        $status = 0;
-        $stmt->bindParam(":status", $status, PDO::PARAM_INT);
-        $now = time() * 1000;
-        $stmt->bindParam(":now", $now, PDO::PARAM_STR);
-        $stmt->execute();
-        $response["status"] = 1;
-        $response["msg"] = "OK";
+        if($onlinestmt->rowCount() == 0){
+          if(getOnlineStatus($request["to"])){
+            /* 
+            Insert Message (Player is online)
+            */
+            $stmt = $mysql->prepare("INSERT INTO privatemessages (SENDER, RECEIVER, MESSAGE, STATUS, DATE) VALUES (:sender, :to, :msg, :status, :now)");
+            $stmt->bindParam(":sender", $access->uuid, PDO::PARAM_STR);
+            $stmt->bindParam(":to", $uuid, PDO::PARAM_STR);
+            $stmt->bindParam(":msg", $request["send"], PDO::PARAM_STR);
+            $status = 0;
+            $stmt->bindParam(":status", $status, PDO::PARAM_INT);
+            $now = time() * 1000;
+            $stmt->bindParam(":now", $now, PDO::PARAM_STR);
+            $stmt->execute();
+            $response["status"] = 1;
+            $response["msg"] = "OK";
+          } else {
+            $response["status"] = 0;
+            $response["msg"] = "Player currently offline";
+          }
+        } else {
+          /* 
+          Insert Message (Player has app)
+          */
+          $stmt = $mysql->prepare("INSERT INTO privatemessages (SENDER, RECEIVER, MESSAGE, STATUS, DATE) VALUES (:sender, :to, :msg, :status, :now)");
+          $stmt->bindParam(":sender", $access->uuid, PDO::PARAM_STR);
+          $stmt->bindParam(":to", $uuid, PDO::PARAM_STR);
+          $stmt->bindParam(":msg", $request["send"], PDO::PARAM_STR);
+          $status = 0;
+          $stmt->bindParam(":status", $status, PDO::PARAM_INT);
+          $now = time() * 1000;
+          $stmt->bindParam(":now", $now, PDO::PARAM_STR);
+          $stmt->execute();
+          $response["status"] = 1;
+          $response["msg"] = "OK";
+        }
     } else if(isset($request["broadcast"]) && isset($request["message"])){
       if(getOnlinePlayers() > 0){
         $stmt = $mysql->prepare("INSERT INTO privatemessages (SENDER, RECEIVER, MESSAGE, STATUS, DATE) VALUES (:sender, :to, :msg, :status, :now)");
@@ -77,18 +93,18 @@ if(isset($request["token"])){
         $stmt->execute();
         $messages = array();
         while($row = $stmt->fetch()){
-          array_push($messages, array("id" => $row["ID"], "from" => getNameByUUID($row["SENDER"]), "message" => $row["MESSAGE"], "status" => $row["STATUS"], "date" => $row["DATE"]));
+          array_push($messages, array("id" => $row["ID"], "from" => UUIDResolve($row["SENDER"]), "message" => $row["MESSAGE"], "status" => $row["STATUS"], "date" => $row["DATE"]));
         }
         $response["teamchat"] = $messages;
     } else {
         $response["status"] = 1;
         $response["msg"] = "OK";
-        $stmt = $mysql->prepare("SELECT * FROM privatemessages WHERE RECEIVER = :uuid ORDER BY DATE DESC");
+        $stmt = $mysql->prepare("SELECT * FROM privatemessages WHERE RECEIVER = :uuid OR SENDER = :uuid ORDER BY DATE DESC");
         $stmt->bindParam(":uuid", $access->uuid, PDO::PARAM_STR);
         $stmt->execute();
         $messages = array();
         while($row = $stmt->fetch()){
-          array_push($messages, array("id" => $row["ID"], "from" => getNameByUUID($row["SENDER"]), "message" => $row["MESSAGE"], "status" => $row["STATUS"], "date" => $row["DATE"]));
+          array_push($messages, array("id" => $row["ID"], "to" => UUIDResolve($row["RECEIVER"]), "from" => UUIDResolve($row["SENDER"]), "message" => $row["MESSAGE"], "status" => $row["STATUS"], "date" => $row["DATE"]));
         }
         $response["privatemessages"] = $messages;
     }
