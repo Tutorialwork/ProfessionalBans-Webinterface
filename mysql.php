6429c6ffb1904801a5b869028bb9ecf256e0535e
Manuel <tutorialworktv@gmail.com>
2020-03-04 14:13:27 +0100

    Better error handling

--- mysql.php
+++ mysql.php
@@ -9,7 +9,21 @@ $passwort = "";
 /////////////////////////////////////////////////
 try{
     $mysql = new PDO("mysql:host=$host;dbname=$name", $user, $passwort);
+    $mysql->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e){
-    echo "SQL Error: ".$e->getMessage();
+    if($host == "localhost" && $name == "professionalbans" && $user == "root" && $passwort == ""){
+        ?>
+        <div class="error">
+            <h3>Bitte stelle deine MySQL Datenbank in der mysql.php ein.</h3>
+        </div>
+        <?php
+    } else {
+        ?>
+        <div class="error">
+            <h3>Ein Fehler mit der MySQL Datenbank ist aufgetreten.</h3>
+            <p><?php echo $e->getMessage() ?></p>
+        </div>
+        <?php
+    }
 }
  ?>
