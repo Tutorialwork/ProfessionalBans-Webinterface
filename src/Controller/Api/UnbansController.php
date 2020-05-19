8002ac712bd82eb3b25c2b1ce69f37fa7876c68c
Manuel <tutorialworktv@gmail.com>
2020-05-19 15:42:54 +0200

    Added unban request status change

--- src/Controller/Api/UnbansController.php
+++ src/Controller/Api/UnbansController.php
@@ -22,8 +22,6 @@ class UnbansController extends AbstractController{
         $this->bansRepository = $bansRepository;
     }
 
-    //TODO
-
     /**
      * @Route("/api/unbans", name="api.unbans", methods={"POST"})
      */
@@ -36,27 +34,66 @@ class UnbansController extends AbstractController{
             ], 401);
         }
 
-        $openUnbans = $this->unbansRepository->findBy(['Status' => 0]);
-        $closedUnbans = $this->unbansRepository->findBy(['Status' => [1, 2, 3]]);
+        if(!array_key_exists("unban", $request) || !array_key_exists("status", $request)){
+            $openUnbans = $this->unbansRepository->findBy(['Status' => 0]);
+            $closedUnbans = $this->unbansRepository->findBy(['Status' => [1, 2, 3]]);
 
-        foreach ($openUnbans as $unban){
-            $uuid = $this->bansRepository->findOneBy(['UUID' => $unban->getUUID()]);
+            foreach ($openUnbans as $unban){
+                $uuid = $this->bansRepository->findOneBy(['UUID' => $unban->getUUID()]);
 
-            $unban->setUuid($uuid->getName());
+                $unban->setUuid($uuid->getName());
+            }
+
+            foreach ($closedUnbans as $unban){
+                $uuid = $this->bansRepository->findOneBy(['UUID' => $unban->getUUID()]);
+
+                $unban->setUuid($uuid->getName());
+            }
+
+            return $this->json([
+                'unbans' => [
+                    'open' => $openUnbans,
+                    'closed' => $closedUnbans
+                ]
+            ]);
         }
 
-        foreach ($closedUnbans as $unban){
-            $uuid = $this->bansRepository->findOneBy(['UUID' => $unban->getUUID()]);
-
-            $unban->setUuid($uuid->getName());
+        if($request->status == "2"){
+            if(!array_key_exists("date", $request)){
+                return $this->json([
+                    'error' => 'Invalid request'
+                ], 400);
+            }
         }
 
-        return $this->json([
-            'unbans' => [
-                'open' => $openUnbans,
-                'closed' => $closedUnbans
-            ]
-        ]);
+        $unban = $this->unbansRepository->findOneBy(['id' => $request->unban]);
+        if($unban){
+            $unban->setStatus((int)$request->status);
+            $ban = $this->bansRepository->findOneBy(['UUID' => $unban->getUuid()]);
+
+            switch ($request->status){
+                case "1":
+                    $ban->setBanned(0);
+                    break;
+                case "2":
+                    $date = new \DateTime($request->date);
+                    $timestamp = $date->getTimestamp();
+                    $timestamp = $timestamp * 1000;
+                    $ban->setEnd($timestamp);
+                    break;
+            }
+
+            $em = $this->getDoctrine()->getManager();
+            $em->flush();
+
+            return $this->json([
+                'success' => 'Unban request was updated'
+            ]);
+        } else {
+            return $this->json([
+                'error' => 'Unban request not found'
+            ], 404);
+        }
     }
 
 }
